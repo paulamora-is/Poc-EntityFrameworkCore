262af6b63d9921c6f55608a8add568aeee17b1cb
Paula Morais <pmorais78@gmail.com>
2021-05-03 09:15:15 -0300

    Adicionando método para inserir dados em massa

--- Poc.EntityFrameworkCore/Data/ApplicationContext.cs
+++ Poc.EntityFrameworkCore/Data/ApplicationContext.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Poc.EntityFrameworkCore.Console.Domains;
 
 namespace Poc.EntityFrameworkCore.Console.Data
 {
     public class ApplicationContext: DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<PedidoItem> PedidoItems { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Poc.EntityFrameworkCore.Console;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Poc.EntityFrameworkCore.Console;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+
+            .UseLoggerFactory(_logger)
+            .EnableSensitiveDataLogging(); //ver o valor dos parâmetros no console
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Poc.EntityFrameworkCore/Program.cs
+++ Poc.EntityFrameworkCore/Program.cs
@@ -1,30 +1,60 @@
-﻿using Poc.EntityFrameworkCore.Console.Domains;
+﻿using Poc.EntityFrameworkCore.Console.Data;
+using Poc.EntityFrameworkCore.Console.Domains;
+using Poc.EntityFrameworkCore.Console.ValueObjects;
+using console = System;
 
 namespace Poc.EntityFrameworkCore.Console
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            InserirProdutos();
-            
+            InserirDados();
+            InserirDadosEmMassa();
         }
 
-        public static void InserirProdutos()
+        private static void InserirDados()
         {
             var produtos = new Produto
             {
                 Descricao = "Produto Teste",
                 CodigoBarras = "1234567890",
                 Valor = 10m,
-                TipoProduto = ValueObjects.TipoProduto.Embalagem,
+                TipoProduto = TipoProduto.Embalagem,
                 Ativo = true
             };
 
-            using var db = new Data.ApplicationContext();
+            using var db = new ApplicationContext();
             db.Set<Produto>().Add(produtos);
 
             var registros = db.SaveChanges();
+            console.Console.WriteLine($"Total registros {registros}");
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var produtos = new Produto
+            {
+                Descricao = "Produto Teste2",
+                CodigoBarras = "19856567890",
+                Valor = 20m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Fulano",
+                CEP = "11111111",
+                Cidade = "São Paulo",
+                Estado = "SP",
+                Telefone = "9999999999"
+            };
+
+            var db = new ApplicationContext();
+            db.AddRange(produtos, cliente);
+            var registros = db.SaveChanges();
+            console.Console.WriteLine($"Total registros {registros}");
         }
     }
 }
