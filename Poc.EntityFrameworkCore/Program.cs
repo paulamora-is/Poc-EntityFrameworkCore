806b21a97cf237b91913e08719ebfecce4799c28
Paula Morais <pmorais78@gmail.com>
2021-05-04 21:50:27 -0300

    Adicionado método de consultar dados

--- Poc.EntityFrameworkCore/Program.cs
+++ Poc.EntityFrameworkCore/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Poc.EntityFrameworkCore.Console.Data;
 using Poc.EntityFrameworkCore.Console.Domains;
 using Poc.EntityFrameworkCore.Console.ValueObjects;
+using System.Linq;
 using console = System;
 
 namespace Poc.EntityFrameworkCore.Console
@@ -11,6 +12,7 @@ namespace Poc.EntityFrameworkCore.Console
         {
             InserirDados();
             InserirDadosEmMassa();
+            ConsultarDados();
         }
 
         private static void InserirDados()
@@ -51,10 +53,48 @@ namespace Poc.EntityFrameworkCore.Console
                 Telefone = "9999999999"
             };
 
+            var listaClientes = new[]
+            {
+               new Cliente
+               {
+                    Nome = "Teste1",
+                    CEP = "22222222",
+                    Cidade = "São Paulo",
+                    Estado = "SP",
+                    Telefone = "8888888888"
+               },
+
+               new Cliente
+               {
+                    Nome = "Teste2",
+                    CEP = "33333333",
+                    Cidade = "São Paulo",
+                    Estado = "SP",
+                    Telefone = "7777777777"
+               }
+            };
+
             var db = new ApplicationContext();
-            db.AddRange(produtos, cliente);
+            db.AddRange(produtos, cliente); //forma de adicionar tipos de objetos
+            db.AddRange(listaClientes); 
             var registros = db.SaveChanges();
             console.Console.WriteLine($"Total registros {registros}");
+        }
+
+        private static void ConsultarDados()
+        {
+            var db = new ApplicationContext();
+            var clientes = db.Clientes.Where(c => c.Id > 0)
+                .OrderBy(c => c.Id)
+                .ToList();
+            foreach (var cliente in clientes)
+            {
+                console.Console.WriteLine($"Consulta: {cliente.Id}");
+                //db.Clientes.Find(cliente.Id); //retornar registros em memória
+                db.Clientes.FirstOrDefault(c => c.Id == cliente.Id);
+            };
+          
+               
         }
     }
 }
